f5ac7a32fbb1d9bf9132e06f6f2e87a4de65f75e
DidoCode <60291476+DidoCode@users.noreply.github.com>
2020-03-02 23:21:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._Predicate_Party_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+            
+            Func<string, int, bool> isValid = (x,y) => y <= (int)x.Aggregate((a, b) => (char)((int)a + (int)b));
+            Func<string[], Func<string, int, bool>, string> firstValidName = (x, y) => x.FirstOrDefault(a => y(a, n));
+
+            Console.WriteLine(firstValidName(names, isValid));
+        }
+    }
+}
